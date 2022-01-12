a15a23c396207e028c117903853260565f355705
butmocks <butmocks@gmail.com>
2022-01-12 17:29:24 +0200

    sd

--- 1intro/task2TODO/src/scripts/index.js
+++ 1intro/task2TODO/src/scripts/index.js
@@ -1,15 +1,13 @@
-
 import { initTodoListHandlers } from './todoList.js';
 import { renderTasks } from './renderer.js';
 import { getTasksList } from './tasksGateway.js';
 import { setItem } from './storage.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  getTasksList()
-    .then(tasksList => {
-      setItem('tasksList', tasksList)
-      renderTasks();
-  })
+  getTasksList().then(tasksList => {
+    setItem('tasksList', tasksList);
+    renderTasks();
+  });
 
   initTodoListHandlers();
 });
